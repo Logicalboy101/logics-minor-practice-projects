9fa95a02017eefa73edd9741e710cb6b55b37c95
Logicalboy101 <logicalboy101@gmail.com>
2024-09-09 16:33:53 -0700

    small update

--- script/DeployMultiplesOfNumbers.s.sol
+++ script/DeployMultiplesOfNumbers.s.sol
@@ -15,5 +15,3 @@ contract DeployMultiplesOfNumbers is Script {
         return multiplesOfNumbers;
     }
 }
-// https://sepolia.etherscan.io/address/0x4fde2838c56b6d4a2e3f0a38bae9aa4252675a74 RomanNumralConverter
-// https://sepolia.etherscan.io/address/0xf7b81ce5e35c606707940f739fb0196bfd4c9e3b MultiplesOfNumbers
